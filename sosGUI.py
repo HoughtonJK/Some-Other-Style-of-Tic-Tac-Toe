f723ba94b15f31355db6b876c2db14a64376920f
HoughtonJK <141585564+HoughtonJK@users.noreply.github.com>
2024-11-09 16:38:16 -0600

    Add files via upload

---
+++ sosGUI.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+
+class GameGUI:
+  def __init__(self, game, root):
+    self.game = game
+    self.root = root
+    self.root.title("SOS Game")
+
+
+    self.create_window()
+
+  def create_window(self):
+    self.selected_mode = tk.StringVar(value="Simple")
+
+    self.mode_label = tk.Label(self.root, text="Mode:")
+    self.mode_label.grid(row=1, column=0)
+    self.simple_radio = tk.Radiobutton(self.root, text="Simple", variable=self.selected_mode, value="Simple", command=self.game.select_mode)
+    self.simple_radio.grid(row=1, column=1)
+    self.general_radio = tk.Radiobutton(self.root, text="General", variable=self.selected_mode, value="General", command=self.game.select_mode)
+    self.general_radio.grid(row=1, column=2)
+
+    self.board_size_label = tk.Label(self.root, text="Board size:")
+    self.board_size_label.grid(row=2, column=0)
+    self.board_size_entry = tk.Entry(self.root, width=3)
+    self.board_size_entry.grid(row=2, column=1)
+    self.board_size_button = tk.Button(self.root, text="New Game", command=self.game.create_new_game)
+    self.board_size_button.grid(row=2, column=2)
+
+    self.state_label = tk.Label(self.root, text=f"Mode: {self.game.mode}, size: {self.game.board.size}")
+    self.state_label.grid(row=3, column=0, columnspan=2)
+    self.player_label = tk.Label(self.root, text=f"Player: {self.game.turn.color}")
+    self.player_label.grid(row=3, column=2)
+
+    self.selected_red_move = tk.StringVar(value="S")
+    self.selected_red_player_type = tk.StringVar(value="Human")
+    self.red_label = tk.Label(self.root, text="Red:")
+    self.red_label.grid(row=4, column=0)
+    self.red_human_radio = tk.Radiobutton(self.root, text="Human", variable=self.selected_red_player_type, value="Human", command=self.update_red_player_type)
+    self.red_human_radio.grid(row=4, column=1)
+    self.red_comp_radio = tk.Radiobutton(self.root, text="Computer", variable=self.selected_red_player_type, value="Computer", command=self.update_red_player_type)
+    self.red_comp_radio.grid(row=4, column=2)
+    self.red_s_radio = tk.Radiobutton(self.root, text="S", variable=self.selected_red_move, value="S", command=self.update_red_symbol)
+    self.red_s_radio.grid(row=5, column=1)
+    self.red_o_radio = tk.Radiobutton(self.root, text="O", variable=self.selected_red_move, value="O", command=self.update_red_symbol)
+    self.red_o_radio.grid(row=5, column=2)
+
+    self.selected_blue_move = tk.StringVar(value="S")
+    self.selected_blue_player_type = tk.StringVar(value="Human")
+    self.blue_label = tk.Label(self.root, text="Blue:")
+    self.blue_label.grid(row=6, column=0)
+    self.blue_human_radio = tk.Radiobutton(self.root, text="Human", variable=self.selected_blue_player_type, value="Human", command=self.update_blue_player_type)
+    self.blue_human_radio.grid(row=6, column=1)
+    self.blue_comp_radio = tk.Radiobutton(self.root, text="Computer", variable=self.selected_blue_player_type, value="Computer", command=self.update_blue_player_type)
+    self.blue_comp_radio.grid(row=6, column=2)
+    self.blue_s_radio = tk.Radiobutton(self.root, text="S", variable=self.selected_blue_move, value="S", command=self.update_blue_symbol)
+    self.blue_s_radio.grid(row=7, column=1)
+    self.blue_o_radio = tk.Radiobutton(self.root, text="O", variable=self.selected_blue_move, value="O", command=self.update_blue_symbol)
+    self.blue_o_radio.grid(row=7, column=2)
+
+    self.message_label = tk.Label(self.root, text="")
+    self.message_label.grid(row=8, column=0, columnspan=3)
+
+    self.red_score_label = tk.Label(self.root, text=f"Red: {self.game.players[0].score}")
+    self.red_score_label.grid(row=9, column=0)
+    self.blue_score_label = tk.Label(self.root, text=f"Blue: {self.game.players[1].score}")
+    self.blue_score_label.grid(row=9, column=1)
+
+    self.display_board()
+
+  def show_stuff(self):
+    self.state_label.config(text=f"Mode: {self.game.mode}, size: {self.game.board.size}")
+
+  def display_turn(self):
+    self.player_label.config(text=f"Player: {self.game.turn.color}")
+
+  def display_scores(self):
+    self.red_score_label.config(text=f"Red: {self.game.players[0].score}")
+    self.blue_score_label.config(text=f"Blue: {self.game.players[1].score}")
+
+  def display_message(self, message):
+    self.message_label.config(text=f"{message}")
+
+  def clear_message(self):
+    self.message_label.config(text=f"")
+
+  def display_board(self):
+    for i in range(self.game.board.size):
+      for j in range(self.game.board.size):
+        button = tk.Button(self.root, text ="", width=3, height=1, command=lambda i=i, j=j: self.game.place_move(i, j))
+        button.grid(row=i, column=j+3)
+        self.game.board.spaces[i][j] = button
+
+  def clear_board(self):
+    for i in range(self.game.board.size):
+      for j in range(self.game.board.size):
+        self.game.board.spaces[i][j].destroy()
+
+  def update_red_symbol(self):
+    self.game.players[0].change_symbol(self.selected_red_move.get())
+  def update_blue_symbol(self):
+    self.game.players[1].change_symbol(self.selected_blue_move.get())
+  def update_red_player_type(self):
+    self.game.players[0].change_type(self.selected_red_player_type.get())
+  def update_blue_player_type(self):
+    self.game.players[1].change_type(self.selected_blue_player_type.get())
---
+++ sosGame.py
@@ -0,0 +1,240 @@
+import random
+import tkinter as tk
+from sosGUI import GameGUI
+
+class Player:
+  def __init__(self, color):
+    self.color = color
+    self.score = 0
+    self.symbol = 'S'
+    self.type = 'Human' # Alternatively, 'Computer'
+
+  def change_symbol(self, symbol):
+    self.symbol = symbol
+
+  def change_type(self, type):
+    self.type = type
+
+  def get_score(self):
+    return self.score
+
+  def score_point(self):
+    self.score += 1
+
+
+class sosGame:
+  def __init__(self, root):
+    self.root = root
+    self.mode = "Simple"
+    self.players = [Player("Red"), Player("Blue")]
+    self.turn = self.players[0]
+    self.board = sosBoard(self, 0)
+    self.gui = GameGUI(self, self.root)
+
+  def select_mode(self):
+    self.gui.clear_message()
+    self.mode = self.gui.selected_mode.get()
+    self.gui.show_stuff()
+
+  def current_symbol(self):
+    return self.turn.symbol
+
+  def place_move(self, i, j):
+    if (i >= self.board.size or i < 0 or j >= self.board.size or j < 0):
+      self.gui.display_message("Error: Not a valid space")
+      return
+    if self.board.spaces[i][j]['text'] == '':
+      self.gui.clear_message()
+      self.board.set_move(i, j, self.current_symbol())
+      self.score_sos(i, j)
+      self.gui.display_scores()
+      if (self.detect_game_ended()):
+        if (self.determine_winner() != 'tie'):
+          self.gui.display_message(f"Congratulations! {self.determine_winner()} has won the game!")
+        else:
+          self.gui.display_message("The game has ended in a tie!")
+        return
+      self.change_turn()
+    else:
+      self.gui.display_message("Error: Please pick unoccupied space")
+
+  def change_turn(self):
+    self.turn = self.players[1] if self.turn == self.players[0] else self.players[0]
+    self.gui.display_turn()
+    if self.turn.type == 'Computer':
+      self.computer_turn()
+
+  def computer_turn(self):
+    move = self.board.best_move()
+    if (move[2] != ''):
+      i = move[0][0]
+      j = move[0][1]
+      symbol = move[2]
+    else:
+      symbol = random.choice(['S', 'O'])
+      i = random.randint(0,self.board.size - 1)
+      j = random.randint(0,self.board.size - 1)
+      while (not self.board.valid_move(i, j) or not self.board.good_move(i, j, symbol)):
+        symbol = random.choice(['S', 'O'])
+        i = random.randint(0,self.board.size - 1)
+        j = random.randint(0,self.board.size - 1)
+    self.turn.change_symbol(symbol)
+    self.place_move(i, j)
+
+  #change to detect_sos()
+  def score_sos(self, i, j):
+    if self.board.spaces[i][j]['text'] == 'O':
+      if i > 0 and j > 0 and i < self.board.size - 1 and j < self.board.size - 1 and self.board.spaces[i + 1][j + 1]['text'] == 'S' and self.board.spaces[i - 1][j - 1]['text'] == 'S':
+        self.turn.score_point()
+      if i > 0 and j > 0 and i < self.board.size - 1 and j < self.board.size - 1 and self.board.spaces[i + 1][j - 1]['text'] == 'S' and self.board.spaces[i - 1][j + 1]['text'] == 'S':
+        self.turn.score_point()
+      if j > 0 and j < self.board.size - 1 and self.board.spaces[i][j + 1]['text'] == 'S' and self.board.spaces[i][j - 1]['text'] == 'S':
+        self.turn.score_point()
+      if i > 0 and i < self.board.size - 1 and self.board.spaces[i + 1][j]['text'] == 'S' and self.board.spaces[i - 1][j]['text'] == 'S':
+        self.turn.score_point()
+    if self.board.spaces[i][j]['text'] == 'S':
+      if i > 1 and j > 1 and i < self.board.size - 2 and j < self.board.size - 2 and self.board.spaces[i + 1][j + 1]['text'] == 'O' and self.board.spaces[i + 2][j + 2]['text'] == 'S':
+        self.turn.score_point()
+      if j > 1 and i < self.board.size - 2 and self.board.spaces[i + 1][j - 1]['text'] == 'O' and self.board.spaces[i + 2][j - 2]['text'] == 'S':
+        self.turn.score_point()
+      if j < self.board.size - 2 and self.board.spaces[i][j + 1]['text'] == 'O' and self.board.spaces[i][j + 2]['text'] == 'S':
+        self.turn.score_point()
+      if i < self.board.size - 2 and self.board.spaces[i + 1][j]['text'] == 'O' and self.board.spaces[i + 2][j]['text'] == 'S':
+        self.turn.score_point()
+      if i > 1 and j > 1 and self.board.spaces[i - 1][j - 1]['text'] == 'O' and self.board.spaces[i - 2][j - 2]['text'] == 'S':
+        self.turn.score_point()
+      if i > 1 and j < self.board.size - 2 and self.board.spaces[i - 1][j + 1]['text'] == 'O' and self.board.spaces[i - 2][j + 2]['text'] == 'S':
+        self.turn.score_point()
+      if j > 1 and self.board.spaces[i][j - 1]['text'] == 'O' and self.board.spaces[i][j - 2]['text'] == 'S':
+        self.turn.score_point()
+      if i > 1 and self.board.spaces[i - 1][j]['text'] == 'O' and self.board.spaces[i - 2][j]['text'] == 'S':
+        self.turn.score_point()
+
+  def detect_game_ended(self):
+    if self.mode == 'Simple':
+      if (self.players[1].score > 0 or self.players[0].score > 0 or self.board.is_board_full()):
+        return True
+      else:
+        return False
+    else:
+      if (self.board.is_board_full()):
+        return True
+      else:
+        return False
+
+  def determine_winner(self):
+    if (self.players[1].score > self.players[0].score):
+      return self.players[1].color
+    elif (self.players[1].score < self.players[0].score):
+      return self.players[0].color
+    else:
+      return 'tie'
+
+  def create_new_game(self):
+    self.gui.clear_board()
+    self.gui.clear_message()
+    self.board.select_size()
+    self.players[0].score = 0
+    self.players[1].score = 0
+    self.players[0].change_symbol(self.gui.selected_red_move.get())
+    self.players[1].change_symbol(self.gui.selected_blue_move.get())
+    self.turn = self.players[0]
+    self.gui.display_scores()
+    self.gui.display_board()
+    self.gui.show_stuff()
+    if(self.players[0].type == 'Computer'):
+      self.computer_turn()
+
+class sosBoard:
+  def __init__(self, game, size):
+    self.game = game
+    self.size = size
+    self.move_count = 0
+    self.spaces = [[None for _ in range(self.size)] for _ in range(self.size)]
+
+  def select_size(self):
+    new_size = int(self.game.gui.board_size_entry.get()) if self.game.gui.board_size_entry.get() != "" else 8
+    if new_size > 2:
+      self.size = new_size
+      self.spaces = [[None for _ in range(self.size)] for _ in range(self.size)]
+      self.move_count = 0
+    else:
+      self.size = 8
+      self.spaces = [[None for _ in range(self.size)] for _ in range(self.size)]
+      self.game.gui.display_message("Error: Board size too small, must be 3 or more.")
+
+  def set_move(self, i, j, symbol):
+      self.spaces[i][j]['text'] = symbol.upper()
+      self.move_count += 1
+
+  def valid_move(self, i, j):
+    return self.spaces[i][j]["text"] == ''
+
+  def good_move(self, i, j, symbol):
+    return self.detect_sos(i, j, symbol) >= 0
+
+  def best_move(self):
+    position = [0, 0]
+    max_count = 0
+    best_symbol = ''
+    for i in range(self.size):
+      for j in range(self.size):
+        if(self.valid_move(i, j)):
+          count_S = self.detect_sos(i, j, 'S')
+          count_O = self.detect_sos(i, j, 'O')
+          if (count_O > count_S):
+            sos_count = count_O
+            new_symbol = 'O'
+          else:
+            sos_count = count_S
+            new_symbol = 'S'
+          if(sos_count > max_count):
+            max_count = sos_count
+            position = [i, j]
+            best_symbol = new_symbol
+    return [position, max_count, best_symbol]
+
+  def detect_sos(self, i, j, symbol):
+    count = 0
+    if symbol == 'O':
+      if i > 0 and j > 0 and i < self.size - 1 and j < self.size - 1 and self.spaces[i + 1][j + 1]['text'] == 'S' and self.spaces[i - 1][j - 1]['text'] == 'S':
+        count += 1
+      if i > 0 and j > 0 and i < self.size - 1 and j < self.size - 1 and self.spaces[i + 1][j - 1]['text'] == 'S' and self.spaces[i - 1][j + 1]['text'] == 'S':
+        count += 1
+      if j > 0 and j < self.size - 1 and self.spaces[i][j + 1]['text'] == 'S' and self.spaces[i][j - 1]['text'] == 'S':
+        count += 1
+      if i > 0 and i < self.size - 1 and self.spaces[i + 1][j]['text'] == 'S' and self.spaces[i - 1][j]['text'] == 'S':
+        count += 1
+    if symbol == 'S':
+      if i > 1 and j > 1 and i < self.size - 2 and j < self.size - 2 and self.spaces[i + 1][j + 1]['text'] == 'O' and self.spaces[i + 2][j + 2]['text'] == 'S':
+        count += 1
+      if j > 1 and i < self.size - 2 and self.spaces[i + 1][j - 1]['text'] == 'O' and self.spaces[i + 2][j - 2]['text'] == 'S':
+        count += 1
+      if j < self.size - 2 and self.spaces[i][j + 1]['text'] == 'O' and self.spaces[i][j + 2]['text'] == 'S':
+        count += 1
+      if i < self.size - 2 and self.spaces[i + 1][j]['text'] == 'O' and self.spaces[i + 2][j]['text'] == 'S':
+        count += 1
+      if i > 1 and j > 1 and self.spaces[i - 1][j - 1]['text'] == 'O' and self.spaces[i - 2][j - 2]['text'] == 'S':
+        count += 1
+      if i > 1 and j < self.size - 2 and self.spaces[i - 1][j + 1]['text'] == 'O' and self.spaces[i - 2][j + 2]['text'] == 'S':
+        count += 1
+      if j > 1 and self.spaces[i][j - 1]['text'] == 'O' and self.spaces[i][j - 2]['text'] == 'S':
+        count += 1
+      if i > 1 and self.spaces[i - 1][j]['text'] == 'O' and self.spaces[i - 2][j]['text'] == 'S':
+        count += 1
+    return count
+
+  def detect_bad_move(self):
+    if(False):
+      True
+
+  def is_board_full(self):
+      return self.move_count >= self.size * self.size
+
+
+def main():
+  root = tk.Tk()
+  game = sosGame(root)
+  root.mainloop()
+
+main()
